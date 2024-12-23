87da2fcbdf3ee40ec55ea156474c193d69b1e3c9
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-12-24 03:45:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import AddBookForm from './Components/AddBookForm';
+import BookList from './Components/BookList';
+import BookProvider from "./Services/BookProvider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookProvider>
+        <Navbar/>
+        <AddBookForm/>
+        <BookList/>
+      </BookProvider>
     </div>
   );
 }
