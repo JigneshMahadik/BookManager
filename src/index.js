0908c3bd584a45d32b1db958df3dfb0545e3dd3b
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-12-24 03:46:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import AddBookForm from './Components/AddBookForm';
+import BookList from './Components/BookList';
+import BookProvider from "./Services/BookProvider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="App">
+      <BookProvider>
+        <Navbar/>
+        <AddBookForm/>
+        <BookList/>
+      </BookProvider>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
